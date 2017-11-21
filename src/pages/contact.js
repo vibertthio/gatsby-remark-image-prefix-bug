3b2bbafaee5e78068514b496fb131d3f69bebc0c
Vibert Thio <vibertthio@gmail.com>
2017-11-21 16:26:28 +0800

    :tada: first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Contact</h1>
+    <p>
+      <a href="mailto:vibertthio@gmail.com">vibertthio at gmail</a>
+    </p>
+  </div>
+);
