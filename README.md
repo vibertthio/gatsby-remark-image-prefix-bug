1f7683d70719cf1a6f5cb75492251f985e427156
Vibert Thio <vibertthio@gmail.com>
2017-11-21 16:44:05 +0800

    :book: add readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-![projects](./assets/screenshots/projects.png)
+#gatsby-remark-image-prefix-bug
+
+##dev in local: work
+![](./assets/screenshots/dev-console.png)
+![](./assets/screenshots/local-work.png)
+
+
+##deploy to gh-pages: wrong prefix
+![](./assets/screenshots/deploy-console.png)
+![](./assets/screenshots/gh-pages-error.png)
+
+
+##deploy after clean cache: work
+![](./assets/screenshots/clean-cache-console.png)
+![](./assets/screenshots/clean-cache-work.png)
--- clean.sh
+++ clean.sh
@@ -1,13 +1,13 @@
-cd public/
-echo "#### Cleaning all the html, js relevent files in ./public.."
-
-find . -name "*.css.map" -type f -delete
-find . -name "*.css" -type f -delete
-find . -name "*.html" -type f -delete
-find . -name "*.json" -type f -delete
-find . -name "*.js.map" -type f -delete
-find . -name "*.js" -type f -delete
-cd ..
+# cd public/
+# echo "#### Cleaning all the html, js relevent files in ./public.."
+#
+# find . -name "*.css.map" -type f -delete
+# find . -name "*.css" -type f -delete
+# find . -name "*.html" -type f -delete
+# find . -name "*.json" -type f -delete
+# find . -name "*.js.map" -type f -delete
+# find . -name "*.js" -type f -delete
+# cd ..
 
 echo "#### Cleaning cache"
 rm -rf ./.cache/
